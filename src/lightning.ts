91e6ce9671ada3286066eb1320d04f66c4f5beaf
AaronDewes <aaron.dewes@protonmail.com>
2022-10-21 11:03:33 +0000

    Timestamps

--- src/lightning.ts
+++ src/lightning.ts
@@ -3,15 +3,35 @@ import * as db from "./db.ts";
 
 const ln = new Lightning(Deno.env.get("CORE_LN_SOCKET") || "/c-lightning/rpc");
 
-export function getMessage() {
-  return "Login for Citadel accounts. Do not sign this message if you are not trying to login on account.runcitadel.space. We never ask you to sign this via chat/email.";
+export function getMessage(previous = false) {
+  let text = `Login for Citadel accounts. Do not sign this message if you are not trying to login on account.runcitadel.space or the Citadel TLS app. We never ask you to sign this via chat/email.`;
+  // Increment time every 10 minutes
+  let time = Math.floor(Date.now() / 1000 / 60 / 10);
+  if (previous) {
+    time -= 1;
+  }
+  // Add a hint on when this will expire
+  text += ` This message is valid until ${new Date((time + 1) * 1000 * 60 * 10).toLocaleString()}.`;
+  return text;
 }
 
 export async function verifyMessage(zbase: string) {
-  return await ln.checkmessage({
-    message: getMessage(),
-    zbase,
-  });
+    try {
+      const response = await ln.checkmessage({
+        message: getMessage(),
+        zbase,
+      });
+      if (!response.verified) {
+        throw new Error();
+      }
+      return response;
+  } catch {
+    // Also allow previous message
+    return await ln.checkmessage({
+      message: getMessage(true),
+      zbase,
+    });
+  }
 }
 
 export async function signUpFromSignature(zbase: string, password: string, redirectTo?: string) {
